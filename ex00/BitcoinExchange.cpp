de4aa8ec607b5a73a6071ad9b6c6690c4ec39ba4
Hassan Lakhal <hlakhal-@e1r4p8.1337.ma>
2023-12-18 18:13:34 +0100

    test

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,30 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:10:26 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/12/18 15:10:27 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:22:07 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"BitcoinExchange.hpp"
+            
+BitcoinExchange::BitcoinExchange(/* args */)
+{
+}
+
+std::string BitcoinExchange::getFile(std::string name)
+{
+    std::ifstream fileData;
+    fileData.open(name);
+    std::string data ,line; 
+    while (std::getline(fileData,line))
+    {
+        data.append(line + "\n");
+    }
+    return data;   
+}
+
+void BitcoinExchange::dataFile()
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,20 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:10:35 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/12/18 15:17:57 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/12/18 16:53:07 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<iostream>
+#include"BitcoinExchange.hpp"
 
 int main(int ac, char *av[])
 {    
     if (ac == 2)
     {
-        
+        BitcoinExchange obj;
+        std::cout <<  obj.getFile(av[1]) << std::endl; 
     }
     else
         std::cerr << "Error: could not open file." << std::endl;
-    return 1; 
+    return 0; 
 }
