f59971e6a420cfaa6e24108e664c63bc699ebd8a
Hassan Lakhal <hlakhal-@e1r4p8.1337.ma>
2023-12-29 17:33:35 +0100

    done ex01

---
+++ ex01/RPN.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/29 14:39:19 by hlakhal-          #+#    #+#             */
+/*   Updated: 2023/12/29 17:32:53 by hlakhal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"RPN.hpp"
+
+RPN::RPN()
+{
+    
+}
+
+RPN::~RPN()
+{
+
+}
+
+void RPN::splitString(const std::string& strToken) 
+{
+    std::istringstream stream(strToken);
+    std::string token;
+    while (std::getline(stream, token, ' ')) 
+        tokens.push_back(token);
+}
+
+int RPN::performOperation(int number1, int number2, char op) 
+{
+    switch (op) 
+    {
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        case '*':
+            return number1 * number2;
+        case '/':
+            if (number2 == 0)
+                throw std::runtime_error("Division by zero");
+            return number1 / number2;
+        default:
+            throw std::invalid_argument("Invalid operator");
+    }
+}
+
+void RPN::evaluateRPN(std::string& strArg)
+{
+    std::string operationsToken = "+-/*";
+    splitString(strArg);
+    std::list<std::string>::iterator it = tokens.begin();
+    while (it != tokens.end())
+    {
+        std::string number(*it);
+        if (number.length() == 1 && isdigit(number[0]))
+        {
+            int digit = atoi(number.c_str());
+            rpnList.push(digit);
+        }
+        else if (number.length() == 2 && number[0] == '-' && isdigit(number[1]))
+        {
+            int digit = atoi(number.c_str());
+            rpnList.push(digit);
+        }
+        else if (number.length() == 1 && std::find(std::begin(operationsToken),std::end(operationsToken),number[0]) != std::end(operationsToken))
+        {
+            if (rpnList.size() < 2)
+                throw std::runtime_error(" Error :size of stack !! ");
+            int number1 = rpnList.top();
+            rpnList.pop();
+            int number2 = rpnList.top();
+            rpnList.pop();
+            int result =  performOperation(number2,number1,number[0]);
+            rpnList.push(result);
+        }
+        it++;
+    }
+    std::cout << rpnList.top() << std::endl;
+}
---
+++ ex01/RPN.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/29 14:39:03 by hlakhal-          #+#    #+#             */
+/*   Updated: 2023/12/29 17:10:50 by hlakhal-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+#include<stack>
+#include <list>
+#include <sstream>
+#include <cctype> 
+#include <stdexcept>  
+class RPN
+{
+    private:
+        std::stack<int> rpnList;
+        std::list<std::string> tokens;
+    public:
+        RPN();
+        ~RPN();
+        void evaluateRPN(std::string& strArg);
+        void splitString(const std::string& strToken);
+        int performOperation(int operand1, int operand2, char op);
+};
